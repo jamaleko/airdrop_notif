2755f373fef267b5d804a2e3ad760de998ec4dd1
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-29 22:42:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,70 @@
 from sources.airdropsio import scrape_airdropsio
 from sources.cryptorank import scrape_cryptorank
 
-items = []
+from telegram import send_message
+from telegram import load_seen
+from telegram import save_seen
+from telegram import build_message
 
-items.extend(
-    scrape_airdropsio()
-)
+from config import BOT_TOKEN
+from config import CHANNEL_ID
 
-items.extend(
-    scrape_cryptorank()
-)
 
-print(
-    len(items)
-)
+def main():
+
+    seen = load_seen()
+
+    items = []
+
+    items.extend(
+        scrape_airdropsio()
+    )
+
+    items.extend(
+        scrape_cryptorank()
+    )
+
+    sent = 0
+
+    for item in items:
+
+        link = item["link"]
+
+        if link in seen:
+            continue
+
+        msg = build_message(item)
+
+        try:
+
+            send_message(
+                BOT_TOKEN,
+                CHANNEL_ID,
+                msg,
+            )
+
+            seen[link] = True
+
+            sent += 1
+
+            print(
+                "sent:",
+                item["title"]
+            )
+
+        except Exception as e:
+
+            print(
+                "telegram error:",
+                e
+            )
+
+    save_seen(seen)
+
+    print(
+        f"new sent: {sent}"
+    )
+
+
+if __name__ == "__main__":
+    main()
